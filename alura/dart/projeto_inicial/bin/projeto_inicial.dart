593162f1ba3bb4b6aa5c892b9b7154d063544453
leon junior <leondodejr@gmail.com>
2023-03-23 11:30:59 -0300

    trabs

---
+++ alura/dart/projeto_inicial/bin/projeto_inicial.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+ while(true){
+  print("-" *30);
+  print("Cadastrar jogo");
+  print("1. Listar Jogo");
+  print("2. Adicinar Jogo");
+  print("3. Excluir jogo");
+  print("4. Procurar por Nome");
+  print("5. Sair");
+  print('Please enter your name: ');
+
+  var select = stdin.readLineSync();
+
+  print(select);
+ 
+
+
+
+
+ }
+
+
+}
+
+
+ 
