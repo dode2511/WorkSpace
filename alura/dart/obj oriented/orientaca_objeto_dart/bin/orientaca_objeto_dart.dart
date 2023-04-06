8a6c661370ca89d0d7d7e198604c4072c99dfacc
leon junior <leondodejr@gmail.com>
2023-04-06 12:17:06 -0300

    dada

--- alura/dart/obj oriented/orientaca_objeto_dart/bin/orientaca_objeto_dart.dart
+++ alura/dart/obj oriented/orientaca_objeto_dart/bin/orientaca_objeto_dart.dart
@@ -1,28 +1,14 @@
-
+import 'dart:io';
 
 void main() {
 
-
-print(funcDevolve('-----'));
-
-
-
-}
-
-
-
-
-String funcDevolve(String str){
-
-
-
-if(str.length > 3){
-
-
-return  'devolvi';
-} else {
-  
-  return 'deu merda';
-
+String? inputString = stdin.readLineSync();
+if (inputString != null && inputString.isNotEmpty) {
+  List<String> input = inputString.split(" ");
+  int line1 = int.parse(input[0]);
+  int line2 = int.parse(input[1]);
+  int line3 = int.parse(input[2]);
+  double result = line1 / (line2 + line3);
+  print(result);
 }
 }
